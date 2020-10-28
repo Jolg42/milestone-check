d7ab0de41167b623fde1b050a5bc7fccd1bfbfcc
Joël <joel.galeran@gmail.com>
2020-10-28 18:17:26 +0100

    github.context

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,19 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
-import {WebhookPayload} from '@actions/github/lib/interfaces';
-
-interface Payload extends WebhookPayload {
-  // eslint-disable-next-line camelcase
-  pull_request?: {
-    [key: string]: any;
-    number: number;
-    html_url?: string;
-    body?: string;
-    milestone?: any;
-  };
-  issue?: {
-    [key: string]: any;
-    number: number;
-    html_url?: string;
-    body?: string;
-    milestone?: any;
-  };
-}
-
 async function run() {
   const {
     payload: {pull_request, issue}
   } = github.context;
+
+  console.log(github.context);
 
   // Do nothing if its not a pr or issue
   const isIssue: boolean = Boolean(issue);
   const isPR: boolean = Boolean(pull_request);
   if (!isIssue && !isPR) {
     console.log(
-      'The event that triggered this action was not a pull request, skipping.'
+      'The event that triggered this action was not an issue or pull request, skipping.'
     );
     return;
   }
