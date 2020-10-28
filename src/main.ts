0b44c1b01382a52f466eba5641ec9e5a968cba19
Joël <joel.galeran@gmail.com>
2020-10-28 18:12:05 +0100

    test :)

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,35 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
+import {WebhookPayload} from '@actions/github/lib/interfaces';
+
+interface Payload extends WebhookPayload {
+  // eslint-disable-next-line camelcase
+  pull_request?: {
+    [key: string]: any;
+    number: number;
+    html_url?: string;
+    body?: string;
+    milestone?: any;
+  };
+  issue?: {
+    [key: string]: any;
+    number: number;
+    html_url?: string;
+    body?: string;
+    milestone?: any;
+  };
+}
+
 async function run() {
   const {
-    payload: {pull_request}
+    payload: {pull_request, issue}
   } = github.context;
 
-  // Do nothing if its not a pr
+  // Do nothing if its not a pr or issue
+  const isIssue: boolean = Boolean(issue);
   const isPR: boolean = Boolean(pull_request);
-  if (!isPR) {
+  if (!isIssue && !isPR) {
     console.log(
       'The event that triggered this action was not a pull request, skipping.'
     );
@@ -16,20 +37,23 @@ async function run() {
   }
 
   console.log(pull_request);
+  console.log(issue);
 
-  if (pull_request?.milestone) {
+  const milestone = issue?.milestone ?? pull_request?.milestone;
+  console.log(milestone);
+  if (milestone) {
     console.log('Milestone set, perfect!');
     return;
   }
 
-  if (!pull_request || !pull_request.user) {
-    console.log(
-      'could not get PR object or PR object did not have user object'
-    );
-    return undefined;
+  const username = issue?.user?.login ?? pull_request?.user?.login;
+  console.log(username);
+  if (!username) {
+    throw Error('Could not get username');
   }
 
-  if (pull_request.user !== 'Renovate') {
+  // Only ask to set the milestone if it's not renovate
+  if (username !== 'renovate-bot') {
     core.setFailed('Please set the milestone!');
   }
 }
